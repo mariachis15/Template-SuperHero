c7e965ba299e740359c4fbdad2dea0faa0287098
Maria Chis <marriaachis@gmail.com>
2024-05-21 17:53:00 +0300

    template CRUD C#

---
+++ Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperHeroApi.Entities;
+
+namespace SuperHeroApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+
+    }
+}
